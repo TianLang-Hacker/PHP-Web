c9c1d2bb26db2b10c8e60410b2dc7f50c3c7ebef
TianLang Hacker <60563003+TianLang-Hacker@users.noreply.github.com>
2024-04-24 16:05:08 +0800

    更新 submitfile.php

--- 2024 04.17/submitfile.php
+++ 2024 04.17/submitfile.php
@@ -1,7 +1,22 @@
 <?php
-if (!empty($_FILES)){
-    foreach ($_FILES['file'] as $name=>$value){
-        echo $name."=".$value."<br>";
-    }
-}
+$upfile=$_FILES['file'] ['tmp_name'];
+$path="uploads/".$_FILES['file'] ['name'];
+$size=$_FILES['file'] ['size'];
+$maxSize=1*1024*1024;
+
+if(!is_uploaded_file($upfile)) {
+	echo '非法上传';
+	exit;
+	}
+
+// if($size>$maxSize){
+// 	echo '文件大小超过1MB';
+// 	exit;
+// }
+
+if(move_uploaded_file($upfile, $path)){
+	echo '文件上传成功';
+	}else{
+		echo '文件上传失败';
+		}
 ?>
